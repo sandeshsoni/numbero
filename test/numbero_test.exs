75749d4cf1d6d31b4144e37fd73a17cbe4fd6cf2
Sandesh Soni <hello@sandeshsoni.com>
2019-03-14 14:05:07 +0530

    add basic test

--- test/numbero_test.exs
+++ test/numbero_test.exs
@@ -3,6 +3,22 @@ defmodule NumberoTest do
   doctest Numbero
 
   test "greets the world" do
-    assert Numbero.hello() == :world
+    # assert Numbero.hello() == :world
   end
+
+
+  test "number to combination" do
+    assert Number.foo(22) contains "AA"
+  end
+
+  test "complete implement 1" do
+    assert Number.foo(6686787825) == [
+      ["motor", "usual"],
+      ["noun", "struck"],
+      ["nouns", "truck"],
+      ["nouns", "usual"],
+      ["onto", "struck"],
+      "motortruck"]
+  end
+
 end
